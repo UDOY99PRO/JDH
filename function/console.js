ebfeea23ec55f9fd4d7f92f6fde2ae332a102243
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 14:37:42 +0530

    Update console.js

--- function/console.js
+++ function/console.js
@@ -1,10 +1,11 @@
 const { WebhookClient } = require('discord.js');
 const webhookClient = new WebhookClient({ url: process.env["console_wh"] });
-
+var Discord = require("discord.js");
 function discord_console(content){
+var embed = new Discord.EmbedBuilder().setColor("#07fc03").setTitle("CONSOLE LOG").setDescription(`\`\`\`${content}\`\`\``).setTimestamp();
 webhookClient.send({
 	username: 'Console Logger',
-	content: `\`\`\`${content}\`\`\``
+	embed: embed
 }).catch({})
 }
 global.discord_console = discord_console;
