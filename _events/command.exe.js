fb118e2db1b3302af269e56388499ae73178e105
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 13:41:18 +0530

    Update command.exe.js

--- _events/command.exe.js
+++ _events/command.exe.js
@@ -2,7 +2,13 @@ module.exports = (client) => {
 var prefix = "-";
 client.on("messageCreate", async(message) => {
 if(!message.content.startsWith(prefix)) { return; }
-
+var args = message.content.slice(prefix.length).trim().split(/ +/);
+var command = args.shift().toLowerCase();
+if(command == "ping"){
+return message.reply(`My Current Pong Is : ${client.ws.ping}`); 
+}else if(command == "uptime"){
+return message.reply(`My Current Uptime Is : ${client.uptime}`); 
+}else{ return; }
 
 });
 }
