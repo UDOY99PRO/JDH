281238ce8331862ef0c8924a791fa548425fff4d
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 19:15:50 +0530

    Update command.exe.js

--- _events/command.exe.js
+++ _events/command.exe.js
@@ -1,23 +1,16 @@
 module.exports= (client) => {
   client.on('messageCreate', async (message) =>{
     if(message.author.bot){	return; }
- /*   var prefix = await mdb.get(`get:guild_${message.guild.id}:_prefix_${client.Slot_Num}_`);
-   
-    if(prefix == null){
-      
-       var prefix = await mdb.get(`get_prefix_${client.Slot_Num}_`);
-  if(prefix == null) var prefix = '?';
-      
-    }*/
+
     var prerix = "-";
- if(!message.content.startsWith(prefix)) return;
+ if(!message.content.startsWith(prefix)){ return; }
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
        const cmd = args.shift().toLowerCase();
         var command = client.commands.get(cmd)
+    console.log(command);
      if(!command) command = client.commands.get(client.aliases.get(cmd));
  if(command){ command.execute(client, message, args); }
-  if(!command){
-    
+  if(!command){   
     setTimeout(() => message.delete(), 8000)
     var nocmd = new Discord.EmbedBuilder()
     .setColor("#ff0808")
