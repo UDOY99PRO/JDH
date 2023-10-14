7e7dc8744fe2fd540cbd8deeb0751972d2e6c643
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 12:45:14 +0530

    Create messageCreate.js

---
+++ _events/messageCreate.js
@@ -0,0 +1,10 @@
+module.exports = (client) => {
+client.on("messageCreate", (message) => {
+  if(message.bot) return;
+  if(message.content == "!ping!"){
+    message.reply({content: `${client.ws.ping}`});
+  }else if(message.content == "!uptime!"){
+        message.reply({content: `${client.uptime}`});
+  }
+});
+});
