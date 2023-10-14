4b43df3e84d0192965dfea9fea8a05290054e07a
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 13:02:08 +0530

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var app = express();
-var server = app.listen(3000, {
-  console.log("web Server started");
-});
+var server = app.listen(3000);
+console.log("web service Started");
 app.get("/", (q, res) => {
   return res.send("Server route /");
 });
