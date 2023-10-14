fb27a830996450c6bc3d996fad6c75f6cef08881
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 13:41:40 +0530

    Update command.exe.js

--- _events/command.exe.js
+++ _events/command.exe.js
@@ -5,9 +5,9 @@ if(!message.content.startsWith(prefix)) { return; }
 var args = message.content.slice(prefix.length).trim().split(/ +/);
 var command = args.shift().toLowerCase();
 if(command == "ping"){
-return message.reply(`My Current Pong Is : ${client.ws.ping}`); 
+return message.reply(`My Current Pong Is : ${client.ws.ping} ms`); 
 }else if(command == "uptime"){
-return message.reply(`My Current Uptime Is : ${client.uptime}`); 
+return message.reply(`My Current Uptime Is : ${client.uptime} ms`); 
 }else{ return; }
 
 });
