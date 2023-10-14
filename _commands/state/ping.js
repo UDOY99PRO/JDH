d16d499b121c816ac1fab9538bee713c71a726c0
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-14 19:34:51 +0530

    Update ping.js

--- _commands/state/ping.js
+++ _commands/state/ping.js
@@ -1,1 +1,14 @@
+module.exports = {
+  name: "ping",
+  aliases: ["pong", "latency"],
+  execute: async(client, message, args) => {
+    var ping = Math.round(client.ws.ping);
+    
+var ping_embed = new Discord.EmbedBuilder()
+    .setColor("#03fc30")
+    .setTitle(`🏓 Pong!`)
+    .setDescription(`\`\`\`js\nBot Latency: ${ping} ms;\`\`\``)
+    .setTimestamp()
 
+    		message.reply({ embeds: [ping_embed]})
+  }}
