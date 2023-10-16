72b96e7475c52a650a4da14536e554f32bba5628
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-16 19:10:09 +0530

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+app.set('view engine', 'ejs');
+
+// Specify the directory where your views are located
+app.set('views', path.join(__dirname, 'views')); // Replace 'views' with your actual views directory
 var server = app.listen(3000);
 console.log("web service Started");
 app.get("/", (q, res) => {
