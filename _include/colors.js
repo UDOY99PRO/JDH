88308974879ace80120b5ab424025bc7b4561e6a
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-16 09:48:18 +0530

    Update colors.js

--- _include/colors.js
+++ _include/colors.js
@@ -1,3 +1,13 @@
-module.exports = {
-green: "green"
-}
+global.color = {
+    red: "#f20000",
+    green: "#72fc00",
+    cyan: "#00fcdf",
+    yellow: "#f4fc00",
+    purple: "#fc00f0",
+    pink: "#fc0394",
+    blue: "#00f0f0",
+    black: "#000000",
+    white: "#ffffff",
+    random: "Random",
+    r: "Random"
+  }
