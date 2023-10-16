96b1997390fea50e38eb137d1306fb5228ebc955
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-16 11:37:30 +0530

    Update mdb.connect.js

--- db/mdb.connect.js
+++ db/mdb.connect.js
@@ -1,1 +1,14 @@
 
+const mongoose = require("mongoose"),
+//colors = require("colors");
+mongoose.set('strictQuery', false);
+  mongoose.connect(process.env["MONGO"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  }).then(async (a, b) => {
+    await console.log(`Connected MongoDB`);
+     global.mongo_conn = true;
+  }).catch((err) => {
+ global.mongo_conn = false;
+   console.log(err)
+    })
