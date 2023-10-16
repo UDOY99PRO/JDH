4e9f00c34608cef04291487d34b516c1d8787d49
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-16 13:54:45 +0530

    Update help.js

--- _commands/general/help.js
+++ _commands/general/help.js
@@ -4,7 +4,7 @@ module.exports = {
   execute: async(client, message, args) => {
   var helpEmbed = new Discord.EmbedBuilder()
     .setColor(color.green).setTitle("Help Menu")
-    .setDescription("---").setTimestamp()
+    .setDescription(`Avalable Commands:\n\n---`).setTimestamp()
     .setFooter({text: `JDH OFFICIAL`})
     message.reply({embeds: [helpEmbed]});
 }}
