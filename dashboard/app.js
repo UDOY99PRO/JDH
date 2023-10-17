8dac5f9dd175154982fc9e44dd69c280eec73a8b
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-17 14:37:24 +0530

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -8,8 +8,20 @@ app
 app.use(express.static(path.join(__dirname, "/public")))
 // Specify the directory where your views are located
 app.set('views', path.join(__dirname, 'views')); // Replace 'views' with your actual views directory
+var apiRoute = require("./routes/api"), 
 var server = app.listen(3000);
 console.log("web service Started");
 app.get("/", (q, res) => {
   return res.render("index");
 });
+app.use("/api", apiRoute);
+
+
+
+
+
+
+
+
+
+
