842dae39aaa78de6a8004584d748476d41ab04fd
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-10-18 13:58:11 +0530

    Update api.js

--- dashboard/routes/api.js
+++ dashboard/routes/api.js
@@ -5,9 +5,10 @@ router.get("/", (q, res) => {
   res.send("Api route");
 });
 
-router.post("/gpt", (req, res) => {
+router.post("/gpt", async(req, res) => {
 var message = req.body.message;
-  
+  var resp = await ask_gpt(message);
+  res.send(resp);
 });
 
 module.exports = router;
