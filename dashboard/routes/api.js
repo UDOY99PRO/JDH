7fddc340bcc318024e6d5e5d48a172bd7e8d6c76
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-11-28 23:19:35 +0530

    Update api.js

--- dashboard/routes/api.js
+++ dashboard/routes/api.js
@@ -129,7 +129,7 @@ var data = "Data is Required";
 })
   .then(response => response.text())
   .then(data => {
-    res.json({success: true, image_url: `data:image/png;base64,${resu}`, html: `<img src="data:image/png;base64,${resu}"/>` format: "png" });
+    res.json({success: true, image_url: `data:image/png;base64,${resu}`, html: `<img src="data:image/png;base64,${resu}"/>`, format: "png" });
   })
   .catch(error => {
      res.json({ success:false, message: 'some error occurred' });
