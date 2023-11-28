5369e5a43ca5c6435ed649af0c1d2ede9063c637
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2023-11-28 12:49:58 +0530

    Update tc.js

--- tc.js
+++ tc.js
@@ -1,4 +1,32 @@
 const args = process.argv.slice(2);
 
-var number = args[1];
-var cc = args[0];
+var rnumber = args[1];
+var rcc = args[0];
+
+async function main(cc, number) {
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Accept-Encoding': 'gzip',
+    'User-Agent': 'Truecaller/11.75.5 (Android;12)',
+    'Authorization': `Bearer a1i0p--jQBd6NFoVp_nPucgWqH2tpi8r9YHANVNwhpml4vgW1XjAxzP5d72qK09j`
+  }
+};
+async function fetchPhoneDetails(cc, number){
+  try{
+  var data = await fetch(`https://search5-noneu.truecaller.com/v2/search?q=${number}&countryCode=${cc}&type=4&placement=SEARCHRESULTS,HISTORY,DETAILS&encoding=json`, requestOptions);
+return data.json();
+  }catch{
+    return "error";
+  }
+}
+
+var rawdata = await fetchPhoneDetails("IN", "+916296557613");  
+ var data = rawdata.data[0]; 
+
+console.log(JSON.stringify(data))
+
+}
+
+main(rcc, rnumber);
